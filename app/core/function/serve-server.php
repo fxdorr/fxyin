97de6fa7ad35f8bb84d5cc5b63c31cbd88c0cf5e
唐启云 <tqy@fxri.net>
2020-05-08 16:12:33 +0800

    更新函数 1.服务器系统

--- app/core/function/serve-server.php
+++ app/core/function/serve-server.php
@@ -24,76 +24,75 @@ function fts_ip()
 
 /**
  * 框架-终端-服务器-系统
- * @param int $size 选择大小写
+ * @param int $type 类型
  * @return mixed
  */
-function fts_system($size = -1)
+function fts_system($type = -1)
 {
-    $agent = php_uname();
+    $echo = php_uname();
     //开始检测
-    if (preg_match("/win/i", $agent) && preg_match("/95/i", $agent)) {
-        $record = 'Windows 95';
-    } else if (preg_match("/win 9x/i", $agent) && preg_match("/4.90/i", $agent)) {
-        $record = 'Windows ME';
-    } else if (preg_match("/win/i", $agent) && preg_match("/98/i", $agent)) {
-        $record = 'Windows 98';
-    } else if (preg_match("/win/i", $agent) && preg_match("/nt 5.1/i", $agent)) {
-        $record = 'Windows XP';
-    } else if (preg_match("/win/i", $agent) && preg_match("/nt 5.2/i", $agent)) {
-        $record = 'Windows 2003';
-    } else if (preg_match("/win/i", $agent) && preg_match("/nt 5/i", $agent)) {
-        $record = 'Windows 2000';
-    } else if (preg_match("/win/i", $agent) && preg_match("/nt/i", $agent)) {
-        $record = 'Windows NT';
-    } else if (preg_match("/win/i", $agent) && preg_match("/32/i", $agent)) {
-        $record = 'Windows 32';
-    } else if (preg_match("/linux/i", $agent)) {
-        $record = 'Linux';
-    } else if (preg_match("/unix/i", $agent)) {
-        $record = 'Unix';
-    } else if (preg_match("/sun/i", $agent) && preg_match("/os/i", $agent)) {
-        $record = 'SunOS';
-    } else if (preg_match("/ibm/i", $agent) && preg_match("/os/i", $agent)) {
-        $record = 'IBM OS/2';
-    } else if (preg_match("/Mac/i", $agent) && preg_match("/PC/i", $agent)) {
-        $record = 'Macintosh';
-    } else if (preg_match("/PowerPC/i", $agent)) {
-        $record = 'PowerPC';
-    } else if (preg_match("/AIX/i", $agent)) {
-        $record = 'AIX';
-    } else if (preg_match("/HPUX/i", $agent)) {
-        $record = 'HPUX';
-    } else if (preg_match("/NetBSD/i", $agent)) {
-        $record = 'NetBSD';
-    } else if (preg_match("/BSD/i", $agent)) {
-        $record = 'BSD';
-    } else if (preg_match("/OSF1/i", $agent)) {
-        $record = 'OSF1';
-    } else if (preg_match("/IRIX/i", $agent)) {
-        $record = 'IRIX';
-    } else if (preg_match("/FreeBSD/i", $agent)) {
-        $record = 'FreeBSD';
-    } else if (preg_match("/teleport/i", $agent)) {
-        $record = 'teleport';
-    } else if (preg_match("/flashget/i", $agent)) {
-        $record = 'flashget';
-    } else if (preg_match("/webzip/i", $agent)) {
-        $record = 'webzip';
-    } else if (preg_match("/offline/i", $agent)) {
-        $record = 'offline';
+    if (preg_match("/win/i", $echo) && preg_match("/95/i", $echo)) {
+        $echo = 'Windows 95';
+    } else if (preg_match("/win 9x/i", $echo) && preg_match("/4.90/i", $echo)) {
+        $echo = 'Windows ME';
+    } else if (preg_match("/win/i", $echo) && preg_match("/98/i", $echo)) {
+        $echo = 'Windows 98';
+    } else if (preg_match("/win/i", $echo) && preg_match("/nt 5.1/i", $echo)) {
+        $echo = 'Windows XP';
+    } else if (preg_match("/win/i", $echo) && preg_match("/nt 5.2/i", $echo)) {
+        $echo = 'Windows 2003';
+    } else if (preg_match("/win/i", $echo) && preg_match("/nt 5/i", $echo)) {
+        $echo = 'Windows 2000';
+    } else if (preg_match("/win/i", $echo) && preg_match("/nt/i", $echo)) {
+        $echo = 'Windows NT';
+    } else if (preg_match("/win/i", $echo) && preg_match("/32/i", $echo)) {
+        $echo = 'Windows 32';
+    } else if (preg_match("/linux/i", $echo)) {
+        $echo = 'Linux';
+    } else if (preg_match("/unix/i", $echo)) {
+        $echo = 'Unix';
+    } else if (preg_match("/sun/i", $echo) && preg_match("/os/i", $echo)) {
+        $echo = 'SunOS';
+    } else if (preg_match("/ibm/i", $echo) && preg_match("/os/i", $echo)) {
+        $echo = 'IBM OS/2';
+    } else if (preg_match("/Mac/i", $echo) && preg_match("/PC/i", $echo)) {
+        $echo = 'Macintosh';
+    } else if (preg_match("/PowerPC/i", $echo)) {
+        $echo = 'PowerPC';
+    } else if (preg_match("/AIX/i", $echo)) {
+        $echo = 'AIX';
+    } else if (preg_match("/HPUX/i", $echo)) {
+        $echo = 'HPUX';
+    } else if (preg_match("/NetBSD/i", $echo)) {
+        $echo = 'NetBSD';
+    } else if (preg_match("/BSD/i", $echo)) {
+        $echo = 'BSD';
+    } else if (preg_match("/OSF1/i", $echo)) {
+        $echo = 'OSF1';
+    } else if (preg_match("/IRIX/i", $echo)) {
+        $echo = 'IRIX';
+    } else if (preg_match("/FreeBSD/i", $echo)) {
+        $echo = 'FreeBSD';
+    } else if (preg_match("/teleport/i", $echo)) {
+        $echo = 'teleport';
+    } else if (preg_match("/flashget/i", $echo)) {
+        $echo = 'flashget';
+    } else if (preg_match("/webzip/i", $echo)) {
+        $echo = 'webzip';
+    } else if (preg_match("/offline/i", $echo)) {
+        $echo = 'offline';
     } else {
-        $record = 'Others';
+        $echo = 'Others';
     }
-    switch ($size) {
+    switch ($type) {
         case 1:
-            $result = strtolower($record);
+            //格式化-小写
+            $echo = strtolower($echo);
             break;
         case 2:
-            $result = strtoupper($record);
-            break;
-        default:
-            $result = $record;
+            //格式化-大写
+            $echo = strtoupper($echo);
             break;
     }
-    return $result;
+    return $echo;
 }
